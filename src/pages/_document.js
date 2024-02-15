6c59475a0f1ab550d2ca734e9130966c0276b158
Stelian Cioarec <cioarecstelian@gmai.com>
2024-02-15 16:00:41 +0200

    Vercel Analytics/Speed Insights

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Document() {
   return (
