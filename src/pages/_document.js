9d98fa68da4aa465d91ffdb38bab823d6e4db049
Stelian Cioarec <cioarecstelian@gmai.com>
2024-02-15 16:02:22 +0200

    Bug fixes.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 export default function Document() {
   return (
